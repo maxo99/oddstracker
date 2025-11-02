3d54bb5020cbb4ee672e29ccb77d08e147445f17
max orr <max.orr@mail.com>
2025-11-02 08:32:17 -0500

    chore: repo cleanup and testing

--- build.sh
+++ build.sh
@@ -1,1 +1,8 @@
-docker build . --no-cache -t oddstracker:latest -t maxo5499/sportsstack-oddstracker:latest
+#!/bin/bash
+set -euo pipefail
+cd "$(dirname "$0")"
+
+docker build . \
+    --no-cache \
+    -t oddstracker:latest \
+    -t maxo5499/sportsstack-oddstracker:latest
--- test/oddstracker/test_oddscollector.py
+++ test/oddstracker/test_oddscollector.py
@@ -4,16 +4,18 @@ from oddstracker.service.oddscollector import collect_and_store_bettingdata
 
 
 @pytest.mark.asyncio
-async def test_odds_collector(mock_betting_data_requests):
+async def test_odds_collector(
+    mock_betting_data_requests,
+):
     toa_result = await collect_and_store_bettingdata(
         provider_key="theoddsapi",
         league="nfl",
-        db_store=False,
+        db_store=True,
     )
     kambi_result = await collect_and_store_bettingdata(
         provider_key="kambi",
         league="nfl",
-        db_store=False,
+        db_store=True,
     )
 
     assert toa_result == {"status": "collected", "events": 0}
